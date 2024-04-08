cb7764a6c3e3b82a2922f7151639beadb9d28638
Justin-Boers <jboers317@gmail.com>
2024-04-08 16:44:43 +0200

    daw
    dwa

---
+++ server/public/javascript/main.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+  }
+  
+  const menuToggle = document.querySelector('.menu-toggle');
+  menuToggle.style.display = 'flex'; // Display the menu toggle button for smaller screens
+  
