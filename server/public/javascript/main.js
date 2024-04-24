2cba45450587d211dcdf81dca1f642a687671d9b
Justin-Boers <jboers317@gmail.com>
2024-04-24 12:56:36 +0200

    update
    update

---
+++ server/public/javascript/main.js
@@ -0,0 +1,20 @@
+container = document.querySelector('.carousel-inner');
+
+const data = [
+    {
+        name: "Afbeelding 1",
+        imageUrl: "https://imgsrv.crunchyroll.com/cdn-cgi/image/format=auto,fit=contain,width=480,height=720,quality=85/catalog/crunchyroll/e108ae17d8d0407417cac40eb52d849a.jpe",
+        episodes: "Episode 1"
+    },
+    {
+        name: "Afbeelding 1",
+        imageUrl: "https://imgsrv.crunchyroll.com/cdn-cgi/image/format=auto,fit=contain,width=480,height=720,quality=85/catalog/crunchyroll/e108ae17d8d0407417cac40eb52d849a.jpe",
+        episodes: "Episode 1"
+    },
+    {
+        name: "Afbeelding 1",
+        imageUrl: "https://imgsrv.crunchyroll.com/cdn-cgi/image/format=auto,fit=contain,width=480,height=720,quality=85/catalog/crunchyroll/e108ae17d8d0407417cac40eb52d849a.jpe",
+        episodes: "Episode 1"
+    },
+]
+
