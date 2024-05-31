543bd06111c38bd11c23b8d52232086ca99ff793
JustinBoers2004 <jboers317@gmail.com>
2024-05-31 09:59:04 +0200

    Revert "update jjk"
    This reverts commit 82701c434a952a5c0b3a290ebed19280aa4078af.

--- server/public/javascript/JujutsuKaisenEpisodescopy.js
+++ server/public/javascript/JujutsuKaisenEpisodescopy.js
@@ -1,39 +1,20 @@
 console.log('episode loaded');
 
-const episodeRowOne = document.querySelector('.episode-row-one');
-const episodeRowTwo = document.querySelector('.episode-row-two');
-const episodeRowThree = document.querySelector('.episode-row-three');
-const episodeRowFour = document.querySelector('.episode-row-four');
-const episodeRowVife = document.querySelector('.episode-row-vife');
-const episodeRowSix = document.querySelector('.episode-row-six');
-
+const episodeRow = document.querySelector('.episode-row');
 const episodeInfo = document.querySelector('.episode-info');
 
+
 fetch('/JujustsuKaisenEpisodes')
-    .then(response => response.json())
-    .then(data => showEpisodes(data))
-    .catch(error => console.error('Error fetching episodes:', error));
+    .then(myData => myData.json())
+    .then(jsonData => showEpisodes(jsonData));
 
 function showEpisodes(episodes) {
     console.log(episodes);
 
-    episodes.forEach(episode => {
-        const episodeNumber = episode.Number; // Gebruik de juiste eigenschap hier
-
-        if (episodeNumber < 9) {
-            episodeRowOne.appendChild(createCard(episode));
-        } else if (episodeNumber < 14) {
-            episodeRowTwo.appendChild(createCard(episode));
-        } else if (episodeNumber < 22) {
-            episodeRowThree.appendChild(createCard(episode));
-        } else if (episodeNumber < 24) {
-            episodeRowFour.appendChild(createCard(episode));
-        } else if (episodeNumber < 29) {
-            episodeRowVife.appendChild(createCard(episode));
-        } else {
-            episodeRowSix.appendChild(createCard(episode));
-        }
-    });
+    for (let i = 0; i < episodes.length; i++) {
+        const episode = episodes[i];
+        episodeRow.appendChild(createCard(episode));
+    }
 }
 
 function createCard(episode) {
@@ -56,19 +37,22 @@ function createCard(episode) {
 }
 
 function fillEpisodeInfoCard(episode) {
+
     const card = `
         <div class="card">
             <h2>${episode.Title}</h2>
             <br>
-            <iframe src="${episode.VideoUrl}" frameborder="0" class="video" allowfullscreen></iframe>
+            <iframe src="${episode.VideoUrl}" frameborder="0"  class="video" allowfullscreen></iframe>
             <br>
             <h3>${episode.Description}</h3>
+            </div>
         </div>`;
 
     episodeInfo.innerHTML = card;
+
 }
 
-// Functie voor scroll-knop
+//function scroll button 
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     if (section) {
